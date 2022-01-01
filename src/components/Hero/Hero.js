c5004a4d4365a0c956ef3be688f2be8cc7eb9778
Brandon <31498965+LordTiger@users.noreply.github.com>
2022-01-01 15:05:22 +0200

    edit: Project Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+<Section row nopadding>
+  <LeftSection>
+    <SectionTitle main center>
+      Hi I am <br /> 
+      Brandon de Bruyn
+    </SectionTitle>
+
+    <SectionText>
+    I'm a South-African based Software Developer, 
+    </SectionText>
+
+    <Button onClick ={() => window.location = 'https://google.com'}> Learn More</Button>
+  </LeftSection>
+</Section>
 );
 
 export default Hero;
