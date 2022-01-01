4ddab7bc0320a69f6049c95ff1413a51868310cd
Brandon <31498965+LordTiger@users.noreply.github.com>
2022-01-01 15:33:31 +0200

    edit: timeline + acomplishments

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
-
-const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
-];
+import { acomplishments } from '../../constants/constants';
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+<Section>
+  <SectionTitle>Personal Acomplishments</SectionTitle>
+  <Boxes>
+    {acomplishments.map((card, index) => (
+      <Box key={index}>
+        <BoxNum>{card.number}+</BoxNum>
+        <BoxText>{card.text}</BoxText>
+      </Box>
+    ))}
+  </Boxes>
+</Section>
 );
 
 export default Acomplishments;
